87d30f9ccbcc75a8a5cf10ee69538c2294df65b1
LeraTormashova <65281894+LeraTormashova@users.noreply.github.com>
2021-09-08 09:49:02 +0300

    PR and rough draft

--- shapes/rectangle.py
+++ shapes/rectangle.py
@@ -3,32 +3,36 @@ Programming for linguists
 
 Implementation of the class Rectangle
 """
+from shapes.shape import Shape
+from math import sqrt
 
 
-class Rectangle:
+class Rectangle(Shape):
     """
     A class for rectangles
     """
     def __init__(self, uid: int, width: int, length: int):
-        pass
+        super().__init__(uid)
+        self.length = length
+        self.width = width
 
     def get_area(self):
         """
         Returns the area of a rectangle
         :return int: the area of a rectangle
         """
-        pass
+        return self.length * self.width
 
     def get_perimeter(self):
         """
         Returns the perimeter of a rectangle
         :return int: the perimeter of a rectangle
         """
-        pass
+        return (self.length + self.width) * 2
 
     def get_diagonal(self):
         """
         Returns the diagonal length  of a rectangle
         :return int: the diagonal length of a rectangle
         """
-        pass
+        return sqrt(self.length ** 2 + self.width ** 2)
--- shapes/shape.py
+++ shapes/shape.py
@@ -12,13 +12,13 @@ class Shape(ABC):
     An abstract class of a shape
     """
     def __init__(self, uid: int):
-        pass
+        self.uid = uid
 
     def get_uid(self):
         """
         Returns the uid of a shape
         """
-        pass
+        return self.uid
 
     @abstractmethod
     def get_area(self):
