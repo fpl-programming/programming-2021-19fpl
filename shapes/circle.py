6b8ee359494524e06c3cffd9b7f3c9c4a89689bb
idontunderstandtbh <65286701+idontunderstandtbh@users.noreply.github.com>
2021-09-08 18:53:05 +0300

    Create pull request

--- shapes/circle.py
+++ shapes/circle.py
@@ -3,32 +3,38 @@ Programming for linguists
 
 Implementation of the class Circle
 """
-
+import math
 
 class Circle:
+    uidCircle = 0
     """
     A class for circles
     """
     def __init__(self, uid: int, radius: int):
-        pass
+        self.uid = uid
+        self.radius = radius
+        Circle.uidCircle = 0
 
     def get_area(self):
         """
         Returns the area of a circle
         :return int: the area of a circle
         """
-        pass
+        circle_area = math.pi*(self.radius**2)
+        return circle_area
 
     def get_perimeter(self):
         """
         Returns the perimeter of a circle
         :return int: the perimeter of a circle
         """
-        pass
+        circle_perimeter = 2*math.pi*self.radius
+        return circle_perimeter
 
     def get_diameter(self):
         """
         Returns the diameter of a circle
         :return int: the diameter of a circle
         """
-        pass
+        circle_diameter = 2*self.radius
+        return circle_diameter
