32fa60bf2ec5d0b628f63b6c15365ad4d4ea6a7b
mgaranina <mashkaeddy6784@gmail.com>
2021-09-08 23:20:56 +0300

    lil change

--- shapes/circle.py
+++ shapes/circle.py
@@ -4,20 +4,22 @@ Programming for linguists
 Implementation of the class Circle
 """
 
+from math import pi
+
 
 class Circle:
     """
     A class for circles
     """
     def __init__(self, uid: int, radius: int):
-        pass
+        self.radius = radius
 
     def get_area(self):
         """
         Returns the area of a circle
         :return int: the area of a circle
         """
-        pass
+        return pi * self.radius ** 2
 
     def get_perimeter(self):
         """
