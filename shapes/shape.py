52c5baf5b158d7cb4763fe2f1896dca83535dd8c
katyleo <65247432+katyleo@users.noreply.github.com>
2021-09-08 16:04:53 +0300

    add shape

--- shapes/shape.py
+++ shapes/shape.py
@@ -12,24 +12,25 @@ class Shape(ABC):
     An abstract class of a shape
     """
     def __init__(self, uid: int):
-        pass
+        self.uid = uid
 
     def get_uid(self):
         """
         Returns the uid of a shape
         """
-        pass
+        return self.uid
 
     @abstractmethod
     def get_area(self):
         """
         Returns the area of a shape
         """
-        pass
+        return self.get_area()
 
     @abstractmethod
     def get_perimeter(self):
         """
         Returns the perimeter of a shape
         """
-        pass
+        return self.get_perimeter()
+
