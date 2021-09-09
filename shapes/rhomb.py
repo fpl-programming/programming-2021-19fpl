c9e822ba41e2df63f8a46bcfdfe741a1eb5e3682
biryukovaksenia <65292851+biryukovaksenia@users.noreply.github.com>
2021-09-09 16:35:57 +0300

    rhombus

--- shapes/rhomb.py
+++ shapes/rhomb.py
@@ -7,8 +7,8 @@ import math
 from shapes.rectangle import Rectangle
 
 class Rhomb(Rectangle):
-    def __init__(self, uid : int, length: int, alpha: int):
-        super.__init__(uid, length, length)
+    def __init__(self, uid: int, length: int, alpha: int):
+        super().__init__(uid, length, length)
         self.alpha = alpha
 
     def get_area(self):
@@ -16,30 +16,22 @@ class Rhomb(Rectangle):
         Returns the area of a rhomb
         :return int: the area of a rhomb
         """
-        return self.side**2 * math.sin(self.alpha)
+        return self.length**2 * math.sin(self.alpha)
 
     def get_perimeter(self):
         """
         Returns the perimeter of a rhomb
         :return int: the perimeter of a rhomb
         """
-        return 4 * self.side
+        return 4 * self.length
 
-    def get_big_diagonal(self):
+    def get_diagonal(self):
         """
-        Returns the big diagonal length of a rhomb
-        :return int: the big diagonal length of a rhomb
-        """
-        return self.side * math.sqrt(2 + 2 * math.cos(self.alpha))
-
-    def get_small_diagonal(self):
-        """
-        Returns the small diagonal length of a rhomb
+        Returns the diagonal length of a rhomb
         :return int: the diagonal length of a rhomb
         """
-        return self.side * math.sqrt(2 + 2 * math.cos(180 - self.alpha))
+        big_diagonal = self.length * math.sqrt(2 + 2 * math.cos(self.alpha))
+        small_diagonal = self.length * math.sqrt(2 + 2 * math.cos(180 - self.alpha))
 
-
-
-
+        return big_diagonal, small_diagonal
 
