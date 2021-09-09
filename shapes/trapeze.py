a83f26f51d89c7cf800e65d4f874d3800c51a837
katyleo <65247432+katyleo@users.noreply.github.com>
2021-09-09 14:22:35 +0300

    try

--- shapes/trapeze.py
+++ shapes/trapeze.py
@@ -11,29 +11,29 @@ class RightTrapeze(Shape):
     A class for right-angled triangle
     """
 
-    def __init__(self, uid: int, a_side: int, b_side: int, c_side: int):
+    def __init__(self, uid: int, a_s: int, b_s: int, c_s: int):
         super().__init__(uid)
-        self.a_side = a_side
-        self.b_side = b_side
-        self.c_side = c_side
+        self.a_s = a_s
+        self.b_s = b_s
+        self.c_s = c_s
 
     def get_area(self):
         """
         Returns the area of a triangle
         :return int: the area of a triangle
         """
-        return ((self.a_side + self.b_side) / 2) * math.sqrt(self.c_side ** 2 - ((self.a_side - self.b_side) ** 2) / 4)
+        return ((self.a_s + self.b_s) / 2) * math.sqrt(self.c_s ** 2 - ((self.a_s - self.b_s) ** 2) / 4)
 
     def get_perimeter(self):
         """
         Returns the perimeter of a triangle
         :return int: the perimeter of a triangle
         """
-        return self.a_side + self.b_side + (2 * self.c_side)
+        return self.a_s + self.b_s + (2 * self.c_s)
 
     def get_diagonal(self):
         """
         Returns the diagonal length of a triangle
         :return int: the diagonal length of a triangle
         """
-        return int(math.sqrt(self.a_side ** 2 + self.b_side * self.c_side))
+        return int(math.sqrt(self.a_s ** 2 + self.b_s * self.c_s))
