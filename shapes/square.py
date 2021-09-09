9021b6763c17597de08c58328d58b5a8b93d7abd
mgaranina <mashkaeddy6784@gmail.com>
2021-09-09 20:55:48 +0300

    fix lint

--- shapes/square.py
+++ shapes/square.py
@@ -14,25 +14,20 @@ class Square(Rectangle):
     def __init__(self, uid: int, length: int):
         super().__init__(uid, length, length)
 
-    '''
     def get_area(self):
         """
         Returns the area of a square
         :return int: the area of a square
         """
-        return self.length ** 2
 
     def get_perimeter(self):
         """
         Returns the perimeter of a square
         :return int: the perimeter of a square
         """
-        return 4 * self.length
 
     def get_diagonal(self):
         """
         Returns the diagonal length of a square
         :return int: the diagonal length of a square
         """
-        return sqrt(2 * self.length ** 2)
-    '''
