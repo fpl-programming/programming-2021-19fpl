07512b864e61a680db6261b5cfb63e3934e76610
katyanemzer <65305336+katyanemzer@users.noreply.github.com>
2021-09-10 00:03:12 +0300

    code for 3 shapes

--- shapes/rectangle.py
+++ shapes/rectangle.py
@@ -4,31 +4,35 @@ Programming for linguists
 Implementation of the class Rectangle
 """
 
+from shapes.shape import Shape
 
-class Rectangle:
+class Rectangle (Shape):
     """
     A class for rectangles
     """
     def __init__(self, uid: int, width: int, length: int):
-        pass
+        self.width=width
+        self.length=length
+        super().__init__(uid)
 
     def get_area(self):
         """
         Returns the area of a rectangle
         :return int: the area of a rectangle
         """
-        pass
+        return self.length*self.width
 
     def get_perimeter(self):
         """
         Returns the perimeter of a rectangle
         :return int: the perimeter of a rectangle
         """
-        pass
+        return 2*(self.width+self.length)
 
     def get_diagonal(self):
         """
         Returns the diagonal length  of a rectangle
         :return int: the diagonal length of a rectangle
         """
-        pass
+        return math.sqrt(self.length**2+self.width**2)
+
--- shapes/square.py
+++ shapes/square.py
@@ -3,32 +3,33 @@ Programming for linguists
 
 Implementation of the class Square
 """
+from shapes.rectangle import Rectangle
 
-
-class Square:
+class Square (Rectangle):
     """
     A class for squares
     """
     def __init__(self, uid: int, length: int):
-        pass
+        super().__init__(uid,length,length)
 
     def get_area(self):
         """
         Returns the area of a square
         :return int: the area of a square
         """
-        pass
+        return super().get_area()
 
     def get_perimeter(self):
         """
         Returns the perimeter of a square
         :return int: the perimeter of a square
         """
-        pass
+        return super().get_perimeter()
 
     def get_diagonal(self):
         """
         Returns the diagonal length of a square
         :return int: the diagonal length of a square
         """
-        pass
+        return super().get_diagonal()
+
