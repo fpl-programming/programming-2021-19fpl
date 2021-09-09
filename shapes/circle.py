fe8573df5fd9fc459f4c77bf400f490f5de81842
komratovaanastasiia <65010843+komratovaanastasiia@users.noreply.github.com>
2021-09-09 11:07:51 +0300

    Add circle, rectangle and square

--- shapes/circle.py
+++ shapes/circle.py
@@ -3,32 +3,35 @@ Programming for linguists
 
 Implementation of the class Circle
 """
+from shapes.shape import Shape
+from math import pi
 
 
-class Circle:
+class Circle(Shape):
     """
     A class for circles
     """
     def __init__(self, uid: int, radius: int):
-        pass
+        super().__init__(uid)
+        self.radius = radius
 
     def get_area(self):
         """
         Returns the area of a circle
         :return int: the area of a circle
         """
-        pass
+        return self.radius ** 2 * pi
 
     def get_perimeter(self):
         """
         Returns the perimeter of a circle
         :return int: the perimeter of a circle
         """
-        pass
+        return self.get_diameter() * pi
 
     def get_diameter(self):
         """
         Returns the diameter of a circle
         :return int: the diameter of a circle
         """
-        pass
+        return self.radius * 2
