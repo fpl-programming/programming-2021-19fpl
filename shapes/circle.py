0ccc6edc2f588866303dc9e5cdb7c197cd73fb15
Ekaterina Sizova <ekaterina.esizovakate-ru@yandex.ru>
2021-09-09 15:29:23 +0300

    Add changes

--- shapes/circle.py
+++ shapes/circle.py
@@ -4,32 +4,35 @@ Programming for linguists
 Implementation of the class Circle
 """
 
+import math
 from shapes.shape import Shape
 
 class Circle(Shape):
     """
     A class for circles
     """
+
     def __init__(self, uid: int, radius: int):
         super().__init__(uid)
+        self.radius = radius
 
     def get_area(self):
         """
         Returns the area of a circle
         :return int: the area of a circle
         """
-        pass
+        return (self.radius)**2 * math.pi
 
     def get_perimeter(self):
         """
         Returns the perimeter of a circle
         :return int: the perimeter of a circle
         """
-        pass
+        return 2 * self.radius * math.pi
 
     def get_diameter(self):
         """
         Returns the diameter of a circle
         :return int: the diameter of a circle
         """
-        pass
+        return 2 * self.radius
--- shapes/square.py
+++ shapes/square.py
@@ -4,31 +4,34 @@ Programming for linguists
 Implementation of the class Square
 """
 
+import math
+from shapes.shape import Shape
 
-class Square:
+class Square(Shape):
     """
     A class for squares
     """
     def __init__(self, uid: int, length: int):
-        pass
+        super().__init__(uid)
+        self.length = length
 
     def get_area(self):
         """
         Returns the area of a square
         :return int: the area of a square
         """
-        pass
+        return self.length ** 2
 
     def get_perimeter(self):
         """
         Returns the perimeter of a square
         :return int: the perimeter of a square
         """
-        pass
+        return self.length * 4
 
     def get_diagonal(self):
         """
         Returns the diagonal length of a square
         :return int: the diagonal length of a square
         """
-        pass
+        return self.length * math.sqrt(2)
