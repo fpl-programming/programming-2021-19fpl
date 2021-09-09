1cb9b0a0ded0fa86453495c42bf5b49fe6951517
biryukovaksenia <65292851+biryukovaksenia@users.noreply.github.com>
2021-09-09 16:23:01 +0300

    new

--- shapes/rhomb.py
+++ shapes/rhomb.py
@@ -12,16 +12,16 @@ class Rhomb(Rectangle):
         self.alpha = alpha
 
     def get_area(self):
-        return side**2 * math.sin(alpha)
+        return self.side**2 * math.sin(self.alpha)
 
     def get_perimeter(self):
-        return 4 * side
+        return 4 * self.side
 
     def get_big_diagonal(self):
-        return side * math.sqrt(2 + 2 * math.cos(alpha))
+        return self.side * math.sqrt(2 + 2 * math.cos(self.alpha))
 
     def get_small_diagonal(self):
-        return side * math.sqrt(2 + 2 * math.cos(180 - alpha))
+        return self.side * math.sqrt(2 + 2 * math.cos(180 - self.alpha))
 
 
 
