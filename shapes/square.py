1fc82bbda4cf646f419de3b345be7c40d2126c81
mgaranina <mashkaeddy6784@gmail.com>
2021-09-09 21:16:45 +0300

    fix errors

--- shapes/square.py
+++ shapes/square.py
@@ -13,21 +13,3 @@ class Square(Rectangle):
     """
     def __init__(self, uid: int, length: int):
         super().__init__(uid, length, length)
-
-    def get_area(self):
-        """
-        Returns the area of a square
-        :return int: the area of a square
-        """
-
-    def get_perimeter(self):
-        """
-        Returns the perimeter of a square
-        :return int: the perimeter of a square
-        """
-
-    def get_diagonal(self):
-        """
-        Returns the diagonal length of a square
-        :return int: the diagonal length of a square
-        """
