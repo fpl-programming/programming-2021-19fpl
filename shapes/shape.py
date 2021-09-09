88e7d691e6bf1cf3f0e712f7046e238689221e34
Artyukhm <mayasergeevna1@gmail.com>
2021-09-09 22:33:21 +0300

    update

--- shapes/shape.py
+++ shapes/shape.py
@@ -7,30 +7,29 @@ Implementation of the abstract class Shape
 from abc import ABC, abstractmethod
 
 
-
 class Shape(ABC):
     """
     An abstract class of a shape
     """
     def __init__(self, uid: int):
-        pass
+        self.uid = uid
 
     def get_uid(self):
         """
         Returns the uid of a shape
         """
-        pass
+        return self.uid
 
     @abstractmethod
     def get_area(self):
         """
         Returns the area of a shape
         """
-        pass
+        return self.get_area
 
     @abstractmethod
     def get_perimeter(self):
         """
         Returns the perimeter of a shape
         """
-        pass
+        return self.get_perimeter
--- shapes/square.py
+++ shapes/square.py
@@ -4,31 +4,35 @@ Programming for linguists
 Implementation of the class Square
 """
 
+from shapes.shape import Shape
+from math import sqrt
 
-class Square:
+
+class Square(Shape):
     """
     A class for squares
     """
     def __init__(self, uid: int, length: int):
-        pass
+        super().__init__(uid)
+        self.length = length
 
     def get_area(self):
         """
         Returns the area of a square
         :return int: the area of a square
         """
-        pass
+        return self.length ** 2
 
     def get_perimeter(self):
         """
         Returns the perimeter of a square
         :return int: the perimeter of a square
         """
-        pass
+        return self.length * 4
 
     def get_diagonal(self):
         """
         Returns the diagonal length of a square
         :return int: the diagonal length of a square
         """
-        pass
+        return sqrt(2 * self.length ** 2)
