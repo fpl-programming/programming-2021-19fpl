f7325b2bab259e3eea62c6f5c8eeb23d9b32c9f6
biryukovaksenia <65292851+biryukovaksenia@users.noreply.github.com>
2021-09-09 18:39:26 +0300

    tests

--- shapes/rhombus.py
+++ shapes/rhombus.py
@@ -6,9 +6,9 @@ Implementation of the abstract class Rhomb
 import math
 from shapes.rectangle import Rectangle
 
-class Rhomb(Rectangle):
+class Rhombus(Rectangle):
     """
-    A class for rhomb
+    A class for rhombus
     """
     def __init__(self, uid: int, length: int, alpha: int):
         super().__init__(uid, length, length)
@@ -16,22 +16,22 @@ class Rhomb(Rectangle):
 
     def get_area(self):
         """
-        Returns the area of a rhomb
-        :return int: the area of a rhomb
+        Returns the area of a rhombus
+        :return int: the area of a rhombus
         """
         return self.length**2 * math.sin(self.alpha)
 
     def get_perimeter(self):
         """
-        Returns the perimeter of a rhomb
-        :return int: the perimeter of a rhomb
+        Returns the perimeter of a rhombus
+        :return int: the perimeter of a rhombus
         """
         return 4 * self.length
 
     def get_diagonal(self):
         """
-        Returns the diagonal length of a rhomb
-        :return int: the diagonal length of a rhomb
+        Returns the diagonal length of a rhombus
+        :return int: the diagonal length of a rhombus
         """
         big_diagonal = self.length * math.sqrt(2 + 2 * math.cos(self.alpha))
         small_diagonal = self.length * math.sqrt(2 + 2 * math.cos(180 - self.alpha))
---
+++ shapes/rhombus_test.py
@@ -0,0 +1,37 @@
+"""
+Programming for linguists
+
+Tests for Rhombus class.
+"""
+
+import unittest
+from shapes.rhombus import Rhombus
+
+class RhombusTestCase(unittest.TestCase):
+    """
+    Creates a
+    This Case of tests checks the functionality of the implementation of Parallelogram
+    """
+    def test_rhombus_get_uid(self):
+        """
+        Creates a rhombus.
+        Tests that the correct uid is returned.
+        """
+        rhombus = Rhombus(0, 6, 90)
+        self.assertEqual(rhombus.get_uid(), 0)
+
+    def test_rhombus_get_area(self):
+        """
+        Creates a rhombus.
+        Tests that the area is calculated correctly.
+        """
+        rhombus = Rhombus(0, 6, 90)
+        self.assertEqual(rhombus.get_area(), 32.18387988962008)
+
+    def test_get_giagonal(self):
+        """
+        Returns the diagonal length  of a rectangle
+        :return int: the diagonal length of a rectangle
+        """
+        rhombus = Rhombus(0, 6, 90)
+        self.assertEqual(rhombus.get_diagonal(),(6.303863865812756, 6.303863865812756))
