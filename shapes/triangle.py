f766ce3c76e934e669715ecf50534ba5163b1dc4
katyanemzer <65305336+katyanemzer@users.noreply.github.com>
2021-09-11 00:21:36 +0300

    mod

---
+++ shapes/triangle.py
@@ -0,0 +1,45 @@
+"""
+Programming for linguists
+
+Implementation of the class Triangle
+"""
+import math
+from shapes.shape import Shape
+
+class Triangle (Shape):
+    """
+    A class for triangles
+    """
+    def __init__(self, uid: int, first_s: int, second_s: int, third_s:int):
+        self.first_s=first_s
+        self.second_s=second_s
+        self.third_s = third_s
+        super().__init__(uid)
+
+    def get_perimeter(self):
+        """
+        Returns the perimeter of a triangle
+        :return int: the perimeter of a triangle
+        """
+        return self.first_s+self.second_s+self.third_s
+    def get_area(self):
+        """
+        Returns the area of a triangle
+        :return int: the area of a triangle
+        """
+
+        half_p=self.get_perimeter()/2
+        return math.sqrt(half_p*(self.half_p-self.first_s)
+                         *(self.half_p-self.second_s)
+                         *(self.half_p-self.third_s))
+
+
+    def get_height(self):
+        """
+        Returns the three heights  of a triangle
+
+        """
+
+        return 2*self.get_area()/self.first_s,\
+               2*self.get_area()/self.second_s,\
+               2*self.get_area()/self.third_s
