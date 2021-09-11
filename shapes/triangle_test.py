34f0f734194c4a41b97f39fe4707432632c11462
ashirka <66332800+ashirka@users.noreply.github.com>
2021-09-11 23:01:26 +0300

    Create triangle_test.py

---
+++ shapes/triangle_test.py
@@ -0,0 +1,36 @@
+"""
+Programming for linguists
+
+Tests for Triangle class.
+"""
+
+import unittest
+from shapes.triangle import Triangle
+
+class TriangleTestCase(unittest.TestCase):
+    """
+    This Case of tests checks the functionality of the implementation of Triangle
+    """
+    def test_triangle_get_uid(self):
+        """
+        Creates a triangle.
+        Tests that the correct uid is returned.
+        """
+        triangle = Triangle(0, 5, 6, 5)
+        self.assertEqual(triangle.get_uid(), 0)
+
+    def test_triangle_get_area(self):
+        """
+        Creates a triangle.
+        Tests that the area is calculated correctly.
+        """
+        triangle = Triangle(0, 5, 6, 5)
+        self.assertEqual(triangle.get_area(), 12)
+
+    def test_triangle_get_perimeter(self):
+        """
+        Creates a triangle.
+        Tests that the perimeter is calculated correctly.
+        """
+        triangle = Triangle(0, 5, 6, 5)
+        self.assertEqual(triangle.get_perimeter(), 16)
