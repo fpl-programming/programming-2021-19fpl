60f21e9d57786c5cd68cbb5ed389e5f912c13556
Карабасова Юлия Алексеевна <61205728+juliakarabasova@users.noreply.github.com>
2021-09-11 14:28:19 +0300

    implement all basic shapes

--- shapes/circle.py
+++ shapes/circle.py
@@ -3,32 +3,36 @@ Programming for linguists
 
 Implementation of the class Circle
 """
+from math import pi
+
+from shape import Shape
 
 
-class Circle:
+class Circle(Shape):
     """
     A class for circles
     """
     def __init__(self, uid: int, radius: int):
-        pass
+        super().__init__(uid)
+        self.radius = radius
 
     def get_area(self):
         """
         Returns the area of a circle
         :return int: the area of a circle
         """
-        pass
+        return pi * (self.radius ** 2)
 
     def get_perimeter(self):
         """
         Returns the perimeter of a circle
         :return int: the perimeter of a circle
         """
-        pass
+        return 2 * pi * self.radius
 
     def get_diameter(self):
         """
         Returns the diameter of a circle
         :return int: the diameter of a circle
         """
-        pass
+        return self.radius * 2
--- shapes/rectangle.py
+++ shapes/rectangle.py
@@ -3,32 +3,37 @@ Programming for linguists
 
 Implementation of the class Rectangle
 """
+from math import sqrt
+
+from shape import Shape
 
 
-class Rectangle:
+class Rectangle(Shape):
     """
     A class for rectangles
     """
     def __init__(self, uid: int, width: int, length: int):
-        pass
+        super().__init__(uid)
+        self.width = width
+        self.length = length
 
     def get_area(self):
         """
         Returns the area of a rectangle
         :return int: the area of a rectangle
         """
-        pass
+        return self.width * self.length
 
     def get_perimeter(self):
         """
         Returns the perimeter of a rectangle
         :return int: the perimeter of a rectangle
         """
-        pass
+        return 2 * (self.width + self.length)
 
     def get_diagonal(self):
         """
         Returns the diagonal length  of a rectangle
         :return int: the diagonal length of a rectangle
         """
-        pass
+        return sqrt(self.width ** 2 + self.length ** 2)
