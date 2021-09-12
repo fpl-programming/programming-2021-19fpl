2d8a4c969a2c679c25ac4c4d6b90fc426964fd8e
daniilzelekson <64979241+daniilzelekson@users.noreply.github.com>
2021-09-12 22:25:02 +0300

    fixing square

--- shapes/square.py
+++ shapes/square.py
@@ -6,12 +6,14 @@ Implementation of the class Square
 import math
 from shapes.rectangle import Rectangle
 
+
 class Square(Rectangle):
     """
     A class for squares
     """
+
     def __init__(self, uid: int, length: int):
-        super().__init__(uid, length)
+        super().__init__(uid, length, length)
 
     def get_area(self):
         """
