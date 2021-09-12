fc771af659a9872ba765853be923187786ad4523
Nastya1405 <frolikovanasta@gmail.com>
2021-09-12 18:02:46 +0300

    shape

--- shapes/shape.py
+++ shapes/shape.py
@@ -25,11 +25,11 @@ class Shape(ABC):
         """
         Returns the area of a shape
         """
-        pass
+
 
     @abstractmethod
     def get_perimeter(self):
         """
         Returns the perimeter of a shape
         """
-        pass
+
