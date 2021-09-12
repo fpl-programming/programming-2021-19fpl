529e9e3430f52d75a7cf92b9ca6b35d6d6f9fe14
unknown <nasenichrg@gmail.com>
2021-09-12 18:38:49 +0300

    add

--- shapes/square.py
+++ shapes/square.py
@@ -8,8 +8,8 @@ from shapes.rectangle import Rectangle
 
 class Square(Rectangle):
     """
-        A class for squares
-        """
+     A class for squares
+    """
 
     def __init__(self, uid: int, length: int):
-    super().__init__(uid, length, length)
+      super().__init__(uid, length, length)
