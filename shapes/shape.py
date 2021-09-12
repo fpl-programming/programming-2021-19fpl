fd7b90c934eb8ff8ece6dec937f276644a98cace
unknown <nasenichrg@gmail.com>
2021-09-12 16:51:14 +0300

    shape

--- shapes/shape.py
+++ shapes/shape.py
@@ -12,14 +12,12 @@ class Shape(ABC):
     An abstract class of a shape
     """
     def __init__(self, uid: int):
-        pass
         self.uid = uid
 
     def get_uid(self):
         """
         Returns the uid of a shape
         """
-        pass
         return self.uid
 
     @abstractmethod
@@ -27,11 +25,11 @@ class Shape(ABC):
         """
         Returns the area of a shape
         """
-        pass
+
 
     @abstractmethod
     def get_perimeter(self):
         """
         Returns the perimeter of a shape
         """
-        pass
+
