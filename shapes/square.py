7c29af4725f360de1049ac2426cf20873b7213c8
daniilzelekson <64979241+daniilzelekson@users.noreply.github.com>
2021-09-12 22:16:12 +0300

    fixing mistakes with square module

--- shapes/square.py
+++ shapes/square.py
@@ -6,29 +6,26 @@ Implementation of the class Square
 import math
 from shapes.rectangle import Rectangle
 
-
 class Square(Rectangle):
     """
     A class for squares
     """
-
     def __init__(self, uid: int, length: int):
-        super().__init__(uid)
-        self.length = length
+        super().__init__(uid, length)
 
     def get_area(self):
         """
         Returns the area of a square
         :return int: the area of a square
         """
-        return self.length * 2
+        return self.length ** 2
 
     def get_perimeter(self):
         """
         Returns the perimeter of a square
         :return int: the perimeter of a square
         """
-        return self.length * 4
+        return 4 * self.length
 
     def get_diagonal(self):
         """
