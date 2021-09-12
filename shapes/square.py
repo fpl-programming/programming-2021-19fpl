b7cd9a247bedd51710bc4acae4f06a9b576a53e7
standfromunder <polinapletneva2001@gmail.com>
2021-09-12 12:33:21 +0300

    hope again

--- shapes/square.py
+++ shapes/square.py
@@ -5,7 +5,6 @@ Implementation of the class Square
 """
 
 
-import math
 from shapes.rectangle import Rectangle
 
 
@@ -15,24 +14,3 @@ class Square(Rectangle):
     """
     def __init__(self, uid: int, length: int):
         super().__init__(uid, length, length)
-
-    '''def get_area(self):
-        """
-        Returns the area of a square
-        :return int: the area of a square
-        """
-        return self.length ^ 2
-
-    def get_perimeter(self):
-        """
-        Returns the perimeter of a square
-        :return int: the perimeter of a square
-        """
-        return self.length * 4
-
-    def get_diagonal(self):
-        """
-        Returns the diagonal length of a square
-        :return int: the diagonal length of a square
-        """
-        return math.sqrt(2 * self.length ** 2)'''
