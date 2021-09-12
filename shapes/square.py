4f8a78c47fb6b15b7918dc4bbc982d8c8d4d13d1
aphexqueen <kristinasheshmeneva@yandex.ru>
2021-09-13 02:23:40 +0300

    fixed square.py

--- shapes/square.py
+++ shapes/square.py
@@ -13,26 +13,28 @@ class Square(Rectangle):
     A class for squares
     """
     def __init__(self, uid: int, length: int):
-        super().__init__(uid)
-        self.length = length
+        super().__init__(uid, length, length)
 
     def get_area(self):
         """
         Returns the area of a square
         :return int: the area of a square
         """
-        super().get_area()
+        area = self.length ** 2
+        return area
 
     def get_perimeter(self):
         """
         Returns the perimeter of a square
         :return int: the perimeter of a square
         """
-        super().get_perimeter()
+        perimeter = self.length * 4
+        return perimeter
 
     def get_diagonal(self):
         """
         Returns the diagonal length of a square
         :return int: the diagonal length of a square
         """
-        super().get_diagonal()
+        diagonal = self.length * math.sqrt(2)
+        return diagonal
