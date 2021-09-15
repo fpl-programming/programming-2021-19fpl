22ddfe5b86f6a4f75b90f4e2bb9389bb20b25e53
yukiyukiharu <65291231+yukiyukiharu@users.noreply.github.com>
2021-09-15 12:17:16 +0300

    fix lint

--- shapes/square.py
+++ shapes/square.py
@@ -13,7 +13,7 @@ class Square(Rectangle):
     """
     def __init__(self, uid: int, length: int):
         super().__init__(uid, length, length)
-    
+
     def get_area(self):
         """
         Returns the area of a square
