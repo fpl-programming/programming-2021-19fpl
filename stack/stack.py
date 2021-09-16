ea264c5cd4ed3cbbf9ee440944f120c7ebb9047e
standfromunder <polinapletneva2001@gmail.com>
2021-09-16 18:34:31 +0300

    2 work

--- stack/stack.py
+++ stack/stack.py
@@ -12,30 +12,43 @@ class Stack:
     """
 
     def __init__(self, data: Iterable = None):
-        pass
+        self.data = data
 
     def push(self, element):
         """
         Add the element ‘element’ at the top of stack
         :param element: element to add to stack
         """
+        self.data.append(element)
 
     def pop(self):
         """
         Delete the element on the top of stack
         """
+        if self.empty():
+            raise ValueError
+        else:
+            return self.data.pop(-1)
 
     def top(self):
         """
         Return the element on the top of stack
         :return: the element that is on the top of stack
         """
+        if self.empty():
+            raise ValueError
+        else:
+            return self.data[-1]
 
     def size(self) -> int:
         """
         Return the number of elements in stack
         :return: Number of elements in stack
         """
+        if self.data:
+            return len(self.data)
+        else:
+            return 0
 
     def empty(self) -> bool:
         """
@@ -43,3 +56,7 @@ class Stack:
         :return: True if stack does not contain any elements
                  False if stack contains elements
         """
+        if not self.data:
+            return True
+        else:
+            return False
