c275871ecaeded90625a3b2ebdbd6c4e0b16af1a
Anastasia <avisaichkina@edu.hse.ru>
2021-09-17 09:18:43 +0300

    done stack.py

--- stack/stack.py
+++ stack/stack.py
@@ -12,30 +12,47 @@ class Stack:
     """
 
     def __init__(self, data: Iterable = None):
-        pass
+        if isinstance(data, int) or isinstance(data, str):
+            self.data = data
+        elif isinstance(data, list) or isinstance(data, tuple) or isinstance(data, range):
+            self.data = list(data)
+        elif isinstance(data, type(None)):
+            self.data = []
+        else:
+            raise ValueError
 
     def push(self, element):
         """
         Add the element ‘element’ at the top of stack
         :param element: element to add to stack
         """
+        self.data.append(element)
 
     def pop(self):
         """
         Delete the element on the top of stack
         """
+        if self.empty():
+            raise ValueError
+
+        self.data.pop()
 
     def top(self):
         """
         Return the element on the top of stack
         :return: the element that is on the top of stack
         """
+        if self.empty():
+            raise ValueError
+        
+        return self.data[-1]
 
     def size(self) -> int:
         """
         Return the number of elements in stack
         :return: Number of elements in stack
         """
+        return len(self.data)  # notes=FIXME
 
     def empty(self) -> bool:
         """
@@ -43,3 +60,4 @@ class Stack:
         :return: True if stack does not contain any elements
                  False if stack contains elements
         """
+        return not self.data
