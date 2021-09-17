ae8bce1169a86d600ba943a0bf4d66b35bb20e7d
marina-kaz <isthismeowgoingfrommyheart@gmail.com>
2021-09-17 14:06:36 +0300

    implemented stack

--- stack/stack.py
+++ stack/stack.py
@@ -12,30 +12,41 @@ class Stack:
     """
 
     def __init__(self, data: Iterable = None):
-        pass
+        if data:
+            self.stack = [i for i in data]
+        else:
+            self.stack = []
 
     def push(self, element):
         """
         Add the element ‘element’ at the top of stack
         :param element: element to add to stack
         """
+        self.stack.append(element)
 
     def pop(self):
         """
         Delete the element on the top of stack
         """
+        if not self.stack:
+            raise ValueError
+        self.stack = self.stack[:-1]
 
     def top(self):
         """
         Return the element on the top of stack
         :return: the element that is on the top of stack
         """
+        if not self.stack:
+            raise ValueError
+        return self.stack[-1]
 
     def size(self) -> int:
         """
         Return the number of elements in stack
         :return: Number of elements in stack
         """
+        return len(self.stack) if self.stack else 0
 
     def empty(self) -> bool:
         """
@@ -43,3 +54,4 @@ class Stack:
         :return: True if stack does not contain any elements
                  False if stack contains elements
         """
+        return False if bool(self.stack) else True
