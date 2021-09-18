dde132c30f0182c7f7f7b470184a427912eaee31
yudakovaolesya <ovyudakova@edu.hse.ru>
2021-09-18 19:21:06 +0300

    stack work

--- stack/stack.py
+++ stack/stack.py
@@ -12,30 +12,41 @@ class Stack:
     """
 
     def __init__(self, data: Iterable = None):
-        pass
+        if isinstance(data, list):
+            self.data = data
+        elif isinstance(data, range):
+            self.data = [element for element in data]
+        else:
+            self.data = []
 
     def push(self, element):
         """
         Add the element ‘element’ at the top of stack
         :param element: element to add to stack
         """
+        self.data.append(element)
 
     def pop(self):
         """
         Delete the element on the top of stack
         """
+        if len(self.data) != 0:
+            del self.data[-1]
 
     def top(self):
         """
         Return the element on the top of stack
         :return: the element that is on the top of stack
         """
+        if len(self.data) != 0:
+            return self.data.pop()
 
     def size(self) -> int:
         """
         Return the number of elements in stack
         :return: Number of elements in stack
         """
+        return len(self.data)
 
     def empty(self) -> bool:
         """
@@ -43,3 +54,7 @@ class Stack:
         :return: True if stack does not contain any elements
                  False if stack contains elements
         """
+        if len(self.data) == 0:
+            return True
+        else:
+            return False
