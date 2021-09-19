d0894cbf82bf74ff007f06c1e7780ca80382dfc8
Anastasia <avisaichkina@edu.hse.ru>
2021-09-19 15:13:39 +0300

    done queue (tried it out)

--- queue_/queue_.py
+++ queue_/queue_.py
@@ -14,20 +14,29 @@ class Queue_:
     """
 
     def __init__(self, data: Iterable = ()):
-        pass
+        if isinstance(data, (int, float, complex, str)):
+            self.data = data
+        elif isinstance(data, (list, tuple, range)):
+            self.data = tuple(data)
+        elif isinstance(data, type(None)):
+            self.data = ()
+        else:
+            raise ValueError
 
     def put(self, element):
         """
         Add the element ‘element’ at the end of queue_
         :param element: element to add to queue_
         """
-        pass
+        self.data = (*self.data, element)
 
     def get(self):
         """
         Remove and return an item from queue_
         """
-        pass
+        element = self.data[0]
+        self.data = self.data[1:]
+        return element
 
     def empty(self) -> bool:
         """
@@ -35,18 +44,18 @@ class Queue_:
         :return: True if queue_ does not contain any elements.
                  False if the queue_ contains elements
         """
-        pass
+        return not self.data
 
     def size(self) -> int:
         """
         Return the number of elements in queue_
         :return: Number of elements in queue_
         """
-        pass
+        return len(self.data)
 
     def top(self):
         """
         Return the element on the top of queue_
         :return: the element that is on the top of queue_
         """
-        pass
+        return self.data[0]
