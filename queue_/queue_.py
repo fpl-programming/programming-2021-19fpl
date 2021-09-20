4ab46987d3f13f9ab37e81fff8135faaaed75ab3
komratovaanastasiia <65010843+komratovaanastasiia@users.noreply.github.com>
2021-09-20 07:28:40 +0300

    skip queue

--- queue_/queue_.py
+++ queue_/queue_.py
@@ -21,13 +21,11 @@ class Queue_:
         Add the element ‘element’ at the end of queue_
         :param element: element to add to queue_
         """
-        pass
 
     def get(self):
         """
         Remove and return an item from queue_
         """
-        pass
 
     def empty(self) -> bool:
         """
@@ -35,18 +33,15 @@ class Queue_:
         :return: True if queue_ does not contain any elements.
                  False if the queue_ contains elements
         """
-        pass
 
     def size(self) -> int:
         """
         Return the number of elements in queue_
         :return: Number of elements in queue_
         """
-        pass
 
     def top(self):
         """
         Return the element on the top of queue_
         :return: the element that is on the top of queue_
         """
-        pass
