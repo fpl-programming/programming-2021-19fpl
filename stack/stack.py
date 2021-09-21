60ed3824eb4e2c6db54c7017e300a812b43aad94
daniilzelekson <64979241+daniilzelekson@users.noreply.github.com>
2021-09-21 20:18:45 +0300

    implemented stack realisation

--- stack/stack.py
+++ stack/stack.py
@@ -5,6 +5,13 @@ Implementation of the data structure "Stack"
 
 from typing import Iterable
 
+
+class Node:
+    def __init__(self, val, next_node):
+        self.val = val
+        self.next = next_node
+
+
 #!
 class Stack:
     """
@@ -12,30 +19,35 @@ class Stack:
     """
 
     def __init__(self, data: Iterable = None):
-        pass
-
+        self.top = None
+        self.size = 0
+        
     def push(self, element):
         """
         Add the element ‘element’ at the top of stack
         :param element: element to add to stack
         """
+        self.top = Node(element, self.top)
 
     def pop(self):
         """
         Delete the element on the top of stack
         """
+        self.top = self.top.next_node
 
     def top(self):
         """
         Return the element on the top of stack
         :return: the element that is on the top of stack
         """
+        return self.top.val
 
     def size(self) -> int:
         """
         Return the number of elements in stack
         :return: Number of elements in stack
         """
+        return self.size
 
     def empty(self) -> bool:
         """
@@ -43,3 +55,4 @@ class Stack:
         :return: True if stack does not contain any elements
                  False if stack contains elements
         """
+        return self.top == None
