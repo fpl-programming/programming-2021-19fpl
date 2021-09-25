54fd3519f653e2d20c06f481e81ef1b61b68ac3f
Ashfak Irem <irem.ashfak@mail.ru>
2021-09-25 15:14:55 +0300

    Update queue_.py

--- queue_/queue_.py
+++ queue_/queue_.py
@@ -1,6 +1,5 @@
 """
 Programming for linguists
-
 Implementation of the data structure "Queue"
 """
 
@@ -13,21 +12,26 @@ class Queue_:
     Queue Data Structure
     """
 
-    def __init__(self, data: Iterable = ()):
-        pass
+    def __init__(self, data: Iterable = (), capacity: int = 50):
+        self.data = list(data)
+        self._capacity = capacity
 
     def put(self, element):
         """
         Add the element ‘element’ at the end of queue_
         :param element: element to add to queue_
         """
-        pass
+        if self.full():
+            raise IndexError
+        self.data.append(element)
 
     def get(self):
         """
         Remove and return an item from queue_
         """
-        pass
+        if self.empty():
+            raise IndexError
+        return self.data.pop(0)
 
     def empty(self) -> bool:
         """
@@ -35,18 +39,18 @@ class Queue_:
         :return: True if queue_ does not contain any elements.
                  False if the queue_ contains elements
         """
-        pass
+        return not self.data
 
     def size(self) -> int:
         """
         Return the number of elements in queue_
         :return: Number of elements in queue_
         """
-        pass
+        return len(self.data)
 
     def top(self):
         """
         Return the element on the top of queue_
         :return: the element that is on the top of queue_
         """
-        pass
+        return self.data[0]
