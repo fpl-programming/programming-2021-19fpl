c624b1343174196486529dcf2770aa6dbd80470f
mcgrozov <65295559+mcgrozov@users.noreply.github.com>
2021-09-25 15:43:12 +0300

    three labs at once

--- queue_/queue_.py
+++ queue_/queue_.py
@@ -1,10 +1,16 @@
 """
 Programming for linguists
-
 Implementation of the data structure "Queue"
 """
 
 from typing import Iterable
+
+
+class CapacityError(Exception):
+    """
+    Custom Error
+    Raises when number of elements exceeds capacity
+    """
 
 
 # pylint: disable=invalid-name
@@ -13,21 +19,34 @@ class Queue_:
     Queue Data Structure
     """
 
-    def __init__(self, data: Iterable = ()):
-        pass
+    def __init__(self, data: Iterable = (), capacity_n: int = 0):
+        if not isinstance(capacity_n, int):
+            raise TypeError('Capacity is not int')
+        self.capacity_n = capacity_n
+        try:
+            self.data = list(data)
+        except TypeError:
+            self.data = []
+        else:
+            if self.capacity_n and (self.size() > self.capacity_n):
+                raise CapacityError('Number of elements exceeds capacity')
 
     def put(self, element):
         """
         Add the element ‘element’ at the end of queue_
         :param element: element to add to queue_
         """
-        pass
+        if self.full():
+            raise CapacityError('Queue is already full')
+        self.data.append(element)
 
     def get(self):
         """
         Remove and return an item from queue_
         """
-        pass
+        if self.empty():
+            raise IndexError('Queue is empty')
+        return self.data.pop(0)
 
     def empty(self) -> bool:
         """
@@ -35,18 +54,37 @@ class Queue_:
         :return: True if queue_ does not contain any elements.
                  False if the queue_ contains elements
         """
-        pass
+        return not self.data
 
     def size(self) -> int:
         """
         Return the number of elements in queue_
         :return: Number of elements in queue_
         """
-        pass
+        return len(self.data)
 
     def top(self):
         """
         Return the element on the top of queue_
         :return: the element that is on the top of queue_
         """
-        pass
+        if self.empty():
+            raise IndexError('Queue is empty')
+        return self.data[0]
+
+    def capacity(self) -> int:
+        """
+        Return the max number of elements in queue_
+        :return: Max number of elements in queue_
+        """
+        return self.capacity_n
+
+    def full(self) -> bool:
+        """
+        Return whether queue_ is full or not
+        :return: True if queue_ is full.
+                False if queue_ is not full
+        """
+        if self.size() == self.capacity() and self.capacity_n:
+            return True
+        return False
--- stack/stack.py
+++ stack/stack.py
@@ -1,5 +1,4 @@
 """
-
 Implementation of the data structure "Stack"
 """
 
@@ -12,30 +11,47 @@ class Stack:
     """
 
     def __init__(self, data: Iterable = None):
-        pass
+        if not isinstance(data, Iterable):
+            self.data = []
+        else:
+            self.data = list(data)
+
 
     def push(self, element):
         """
         Add the element ‘element’ at the top of stack
         :param element: element to add to stack
         """
+        self.data.append(element)
+
 
     def pop(self):
         """
         Delete the element on the top of stack
         """
+        if self.empty():
+            raise ValueError
+        self.data.pop()
+
 
     def top(self):
         """
         Return the element on the top of stack
         :return: the element that is on the top of stack
         """
+        if not self.data:
+            raise ValueError
+        return self.data[-1]
+
 
     def size(self) -> int:
         """
         Return the number of elements in stack
         :return: Number of elements in stack
         """
+        return len(self.data)
+
+
 
     def empty(self) -> bool:
         """
@@ -43,3 +59,4 @@ class Stack:
         :return: True if stack does not contain any elements
                  False if stack contains elements
         """
+        return not self.data
