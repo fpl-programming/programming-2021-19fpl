8107686f0ad50f0054bcf95739eb590ef479e339
biryukovaksenia <65292851+biryukovaksenia@users.noreply.github.com>
2021-09-26 13:34:04 +0300

    6

--- queue_/queue_.py
+++ queue_/queue_.py
@@ -1,4 +1,3 @@
-# pylint: skip-file
 """
 Programming for linguists
 
@@ -8,27 +7,26 @@ Implementation of the data structure "Queue"
 from typing import Iterable
 
 
-# pylint: disable=invalid-name
 class Queue_:
     """
     Queue Data Structure
     """
 
     def __init__(self, data: Iterable = ()):
-        pass
+        self.data = list(data)
 
     def put(self, element):
         """
         Add the element ‘element’ at the end of queue_
         :param element: element to add to queue_
         """
-        pass
+        self.data.append(element)
 
     def get(self):
         """
         Remove and return an item from queue_
         """
-        pass
+        self.data.pop(0)
 
     def empty(self) -> bool:
         """
@@ -36,18 +34,18 @@ class Queue_:
         :return: True if queue_ does not contain any elements.
                  False if the queue_ contains elements
         """
-        pass
+        return not self.data
 
     def size(self) -> int:
         """
         Return the number of elements in queue_
         :return: Number of elements in queue_
         """
-        pass
+        return len(self.data)
 
     def top(self):
         """
         Return the element on the top of queue_
         :return: the element that is on the top of queue_
         """
-        pass
+        return self.data[0]
