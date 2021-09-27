c69a6da568469a347fe8319c331f46da57c9cb4e
daniilzelekson <64979241+daniilzelekson@users.noreply.github.com>
2021-09-27 19:19:44 +0300

    lab 3 for 6 mark

--- queue_/queue_.py
+++ queue_/queue_.py
@@ -1,4 +1,3 @@
-# pylint: skip-file
 """
 Programming for linguists
 
@@ -8,27 +7,32 @@ Implementation of the data structure "Queue"
 from typing import Iterable
 
 
-# pylint: disable=invalid-name
-class Queue_:
+class Queue:
     """
     Queue Data Structure
     """
 
-    def __init__(self, data: Iterable = ()):
-        pass
+    def __init__(self, data: Iterable = (), max_size_queue: int = float("inf")):
+        self.data = list(data)
+        self.max_size_queue = max_size_queue
 
     def put(self, element):
         """
         Add the element ‘element’ at the end of queue_
         :param element: element to add to queue_
         """
-        pass
+        self.data.append(element)
 
     def get(self):
         """
         Remove and return an item from queue_
         """
-        pass
+        if self.empty():
+            raise IndexError
+
+        result = self.data[0]
+        del self.data[0]
+        return result
 
     def empty(self) -> bool:
         """
@@ -36,18 +40,20 @@ class Queue_:
         :return: True if queue_ does not contain any elements.
                  False if the queue_ contains elements
         """
-        pass
+        return len(self.data) == 0
 
     def size(self) -> int:
         """
         Return the number of elements in queue_
         :return: Number of elements in queue_
         """
-        pass
+        return len(self.data)
 
     def top(self):
         """
         Return the element on the top of queue_
         :return: the element that is on the top of queue_
         """
-        pass
+        if self.empty():
+            raise IndexError
+        return self.data[0]
