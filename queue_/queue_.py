50210e3ba8496fa4da3ac6c8084e7e6ccb677aa6
katerinamerkulova <merkulova1999@mail.ru>
2021-09-27 16:43:11 +0300

    add queue

--- queue_/queue_.py
+++ queue_/queue_.py
@@ -13,21 +13,31 @@ class Queue_:
     Queue Data Structure
     """
 
-    def __init__(self, data: Iterable = ()):
-        pass
+    def __init__(self, data: Iterable = (), maxlen: int = None):
+        self.maxlen = maxlen
+
+        self.data = list(data)
+        if self.maxlen and self.size() > self.maxlen:
+            self.data = self.data[-maxlen:]
 
     def put(self, element):
         """
         Add the element ‘element’ at the end of queue_
         :param element: element to add to queue_
         """
-        pass
+        if self.maxlen and self.full():
+            first = self.get()
+            self.data.append(element)
+        else:
+            self.data.append(element)
 
     def get(self):
         """
         Remove and return an item from queue_
         """
-        pass
+        if self.empty():
+            raise IndexError('Queue is empty.')
+        return self.data.pop(0)
 
     def empty(self) -> bool:
         """
@@ -35,18 +45,32 @@ class Queue_:
         :return: True if queue_ does not contain any elements.
                  False if the queue_ contains elements
         """
-        pass
+        if not self.data:
+            return True
+        return False
 
     def size(self) -> int:
         """
         Return the number of elements in queue_
         :return: Number of elements in queue_
         """
-        pass
+        return len(self.data)
 
     def top(self):
         """
         Return the element on the top of queue_
         :return: the element that is on the top of queue_
         """
-        pass
+        if self.empty():
+            raise IndexError('Queue is empty.')
+        return self.data[0]
+
+    def full(self):
+        """
+        Return whether queue_ is full or not
+        :return: True if size of queue_ equals maxlen.
+                 False if the queue_ isn't full
+        """
+        if self.size() < self.maxlen:
+            return False
+        return True
