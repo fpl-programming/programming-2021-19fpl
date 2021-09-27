196d6bb8b51d6f0afde9cdf79ba46ac98c9d48ea
daniilzelekson <64979241+daniilzelekson@users.noreply.github.com>
2021-09-27 20:09:41 +0300

    lab 3 fixes

--- queue_/queue_test.py
+++ queue_/queue_test.py
@@ -8,6 +8,8 @@ import unittest
 
 from queue_.queue_ import Queue_
 
+from queue_.stack_queue import StackQueue
+
 
 class QueueTestCase(unittest.TestCase):
     """
@@ -93,4 +95,91 @@ class QueueTestCase(unittest.TestCase):
         Error which is called when size is too big
         """
         queue = Queue_(data=[2, 3], max_size_queue=2)
+        self.assertRaises(IndexError, queue.put, 1)
+
+
+class StackQueueTestCase(unittest.TestCase):
+    """
+    This Case of tests checks the functionality of the implementation of Queue
+    """
+
+    def test_new_queue_is_empty(self):
+        """
+        Create an empty Queue.
+        Test that its size is 0.
+        """
+        queue = StackQueue()
+        self.assertTrue(queue.empty())
+        self.assertEqual(queue.size(), 0)
+
+    def test_get_element(self):
+        """
+        Get an element from a queue.
+        Test that it is 1.
+        """
+        data = (1, 2, 3, 4)
+        queue = StackQueue(data)
+        self.assertEqual(queue.get(), data[0])
+
+    def test_new_queue_from_tuple(self):
+        """
+        Create a Queue from an iterable object.
+        Check that the size of queue_ equals to the size of the given tuple.
+        """
+        data = (1, 2, 3, 4)
+        queue = StackQueue(data)
+        self.assertFalse(queue.empty())
+        self.assertEqual(queue.size(), len(data))
+        for value in data:
+            test_value = queue.get()
+            self.assertEqual(test_value, value)
+        self.assertTrue(queue.empty())
+        self.assertEqual(queue.size(), 0)
+
+    def test_new_queue_from_list(self):
+        """
+        Create a Queue from a list.
+        Check that the size of queue equals to the size of the queue.
+        Check that the top element of queue equals to the latest element of the list.
+        """
+        data = [1, 3, 5, 7, 2, 4]
+        queue = StackQueue(data)
+        self.assertFalse(queue.empty())
+        self.assertEqual(queue.size(), len(data))
+        self.assertEqual(queue.top(), data[0])
+
+    def test_new_queue_from_generator(self):
+        """
+        Create a Queue_ from a generator.
+        Test that its size equals to the number provided in the generator.
+        """
+        queue = StackQueue(range(10))
+        self.assertFalse(queue.empty())
+        self.assertEqual(queue.size(), 10)
+        self.assertEqual(queue.top(), 0)
+
+    def test_put_element(self):
+        """
+        Put an element in queue.
+        Test that its size is 1.
+        """
+        queue = StackQueue()
+        queue.put(1)
+        self.assertFalse(queue.empty())
+        self.assertEqual(queue.size(), 1)
+        self.assertEqual(queue.top(), 1)
+
+    def test_call_get_of_empty_queue_raised_error(self):
+        """
+        Create an empty Queue.
+        Test that call of get function raises Assertion error
+        """
+        queue = StackQueue()
+        self.assertRaises(IndexError, queue.get)
+
+    def test_queue_overflow_raise_error(self):
+        """
+        Error which is called when size is too big
+        """
+        queue = StackQueue(data=[2, 3], max_size_queue=2)
         self.assertRaises(IndexError, queue.put, 1)
---
+++ queue_/stack_queue.py
@@ -0,0 +1,69 @@
+from stack.stack import Stack
+
+"""
+Programming for linguists
+
+Implementation of the data structure "Queue"
+"""
+
+from typing import Iterable
+
+
+# pylint: disable=invalid-name
+class StackQueue:
+    """
+    Queue Data Structure
+    """
+
+    def __init__(self, data: Iterable = (), max_size_queue: int = float("inf")):
+        self.first = Stack(data)
+        self.second = Stack()
+        self.max_size_queue = max_size_queue
+
+    def put(self, element):
+        """
+        Add the element ‘element’ at the end of queue_
+        :param element: element to add to queue_
+        """
+        if self.size() >= self.max_size_queue:
+            raise IndexError
+        return self.first.push(element)
+
+    def get(self):
+        """
+        Remove and return an item from queue_
+        """
+        if self.empty():
+            raise IndexError
+        if self.second.empty():
+            while not self.first.empty():
+                element = self.first.top()
+                self.second.push(element)
+                self.first.pop()
+        element = self.second.top()
+        self.second.pop()
+        return element
+
+    def empty(self) -> bool:
+        """
+        Return whether queue_ is empty or not
+        :return: True if queue_ does not contain any elements.
+                 False if the queue_ contains elements
+        """
+        return self.size == 0
+
+    def size(self) -> int:
+        """
+        Return the number of elements in queue_
+        :return: Number of elements in queue_
+        """
+        return self.first.size() + self.second.size()
+
+    def top(self):
+        """
+        Return the element on the top of queue_
+        :return: the element that is on the top of queue_
+        """
+        if self.empty():
+            raise IndexError
+        return self.second.top()
