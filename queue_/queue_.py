b340fbed46c69e999209727289d4840a0996a9da
katerinamerkulova <merkulova1999@mail.ru>
2021-09-27 16:47:36 +0300

    minor changes

--- queue_/queue_.py
+++ queue_/queue_.py
@@ -14,20 +14,20 @@ class Queue_:
     """
 
     def __init__(self, data: Iterable = ()):
-        pass
+        self.data = list(data)
 
     def put(self, element):
         """
         Add the element ‘element’ at the end of queue_
         :param element: element to add to queue_
         """
-        pass
+        self.data.append(element)
 
     def get(self):
         """
         Remove and return an item from queue_
         """
-        pass
+        return self.data.pop(0)
 
     def empty(self) -> bool:
         """
@@ -35,18 +35,18 @@ class Queue_:
         :return: True if queue_ does not contain any elements.
                  False if the queue_ contains elements
         """
-        pass
+        return False if self.data else True
 
     def size(self) -> int:
         """
         Return the number of elements in queue_
         :return: Number of elements in queue_
         """
-        pass
+        return len(self.data)
 
     def top(self):
         """
         Return the element on the top of queue_
         :return: the element that is on the top of queue_
         """
-        pass
+        return self.data[0]
