acc41801ddde5f4a6536e2d5de8c46189aa75b67
daniilzelekson <64979241+daniilzelekson@users.noreply.github.com>
2021-09-30 00:42:36 +0300

    lab_2 changes

--- stack/stack.py
+++ stack/stack.py
@@ -33,9 +33,6 @@ class StackIterator:
     def __next__(self):
         if self.now_element is None:
             raise StopIteration
-        if self.now_element.get_next() is None:
-            raise StopIteration
-
         element = self.now_element.get_val()
         self.now_element = self.now_element.get_next()
         return element
