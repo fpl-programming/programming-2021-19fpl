50ca64399e5535a4d3f851106afad188d3919320
aphexqueen <kristinasheshmeneva@yandex.ru>
2021-10-05 19:01:29 +0300

    add class for nodes

--- binarytree/node.py
+++ binarytree/node.py
@@ -8,30 +8,36 @@ class Node:
     Auxiliary Node Structure
     """
     def __init__(self, value: int):
-        pass
+        self.value = None
+        if isinstance(value, int):
+            self.value = value
+        self.left = None
+        self.right = None
 
     def add_left(self, value: int):
         """
         Add left descendant of the node
         :param value: the value which is smaller than the value of the node
         """
-        pass
+        if isinstance(value, int):
+            self.left = value
 
     def add_right(self, value: int):
         """
         Add right descendant of the node
         :param value: the value which is greater than the value of the node
         """
-        pass
-
-    def return_left(self):
-        """
-        Return left descendant of the node
-        """
-        pass
-
-    def return_right(self):
-        """
-        Return right descendant of the node
-        """
-        pass
+        if isinstance(value, int):
+            self.right = value
+    #
+    # def return_left(self):
+    #     """
+    #     Return left descendant of the node
+    #     """
+    #     return self.left
+    #
+    # def return_right(self):
+    #     """
+    #     Return right descendant of the node
+    #     """
+    #     return self.right
