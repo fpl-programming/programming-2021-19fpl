def15783d0ac07f8d2485089363b32f07ababb76
katyanemzer <65305336+katyanemzer@users.noreply.github.com>
2021-10-07 01:41:11 +0300

    'mod'

--- binarytree/binarytree.py
+++ binarytree/binarytree.py
@@ -3,39 +3,33 @@ Programming for linguists
 
 Implementation of the data structure "Binarytree"
 """
+
+
 class Node:
     def __init__(self, data):
-        self.data=data
-        self.left_element=None
-        self.right_element=None
+        self.data = data
+        self.left_element = None
+        self.right_element = None
+
 
 class BinaryTree(Node):
-    def __init__(self,tree, root):
-        self.root=None
+    def __init__(self, tree, root):
+        self.root = None
 
-
-    def add(self,element):
+    def add(self, element):
         pass
 
+    def find_recurs(self, node, key):
 
-    def search_recurs (self,node,key):
-        if self.node.data  == self.key or self.node is None:
+        if self.node.data == self.key or self.node is None:
             return self.root
-        elif  self.key<self.node.data:
-            return self.search_recurs(node.left_element,key)
+        elif self.key < self.node.data:
+            return self.find_recurs(node.left_element,key)
         else:
-            return self.search_recurs(node.right_element, key)
-    def remove_recurs:
+            return self.find_recurs(node.right_element, key)
+
+    def remove_recurs(self):
         pass
 
-    def get_height (self):
+    def get_height(self):
         pass
-
-
-
-
-
-    def find (self):
-    def
-
-from binarytree.node import Node
