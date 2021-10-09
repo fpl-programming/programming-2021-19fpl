62e38cfff41b229e54c9aea29cac124b3d68837a
Карабасова Юлия Алексеевна <61205728+juliakarabasova@users.noreply.github.com>
2021-10-09 11:57:06 +0300

    add all the descriptions

--- binary_search/binary_search.py
+++ binary_search/binary_search.py
@@ -6,11 +6,19 @@ Implementation of the data structure "BinarySearchTree"
 
 
 class BinarySearchTree:
+    """
+    BinarySearchTree Structure
+    """
+
     def __init__(self, name: str = None, root: int = None):
         self.name = name
         self._root = Node(root)
 
     def add(self, element: int):
+        """
+        Add the element ‘element’ at the correct place of the tree
+        :param element: element to add to the tree
+        """
         if not self._root.root:
             self._root = Node(element)
             return
@@ -19,6 +27,11 @@ class BinarySearchTree:
         self._add_node(self._root, element)
 
     def _add_node(self, root: 'Node', element: int):
+        """
+        Auxiliary recursive function to find the right place for the element in the tree
+        :param root: current node to search a free place in
+        :param element: element to add to the tree
+        """
         if element < root.root and root.left_node:
             self._add_node(root.left_node, element)
         elif element > root.root and root.right_node:
@@ -32,6 +45,10 @@ class BinarySearchTree:
             return
 
     def find(self, element: int):
+        """
+        Find and return the required node
+        :param element: element to find in the tree
+        """
         current_node = self._root
         while current_node.root != element and current_node.root:
             if element < current_node.root and current_node.left_node:
@@ -43,6 +60,10 @@ class BinarySearchTree:
         return current_node
 
     def remove(self, element: int):
+        """
+        Remove the required node from the tree
+        :param element: element to remove from the tree
+        """
         if self.find(element).root is None:
             return 'No node found to remove'
         if element == self._root.root:
@@ -51,6 +72,11 @@ class BinarySearchTree:
         self._remove_side_node(self._root, element)
 
     def _remove_side_node(self, root: 'Node', element: int):
+        """
+        Auxiliary recursive function to find the right node for removal
+        :param root: current node to search the element in
+        :param element: element to remove from the tree
+        """
         if root.left_node and root.left_node.root == element:
             root.left_node = None
             return
@@ -63,6 +89,9 @@ class BinarySearchTree:
             self._remove_side_node(root.right_node, element)
 
     def get_root(self):
+        """
+        Return the current root of the tree
+        """
         return self._root.root
 
     '''
@@ -80,6 +109,10 @@ class BinarySearchTree:
 
 
 class Node:
+    """
+    Node Data Structure
+    """
+
     def __init__(self, root: int or None, left_node: 'Node' = None, right_node: 'Node' = None):
         self.root = root
         self.left_node = left_node
--- binary_search/binary_search_test.py
+++ binary_search/binary_search_test.py
@@ -15,6 +15,10 @@ class BinarySearchTreeTestCase(unittest.TestCase):
     """
 
     def test_binary_search_tree_add_root(self):
+        """
+        Create an empty BinarySearchTree and add an element.
+        Test that the element becomes root.
+        """
         tree = BinarySearchTree()
         tree.add(8)
         self.assertEqual(tree.get_root(), 8)
@@ -22,23 +26,39 @@ class BinarySearchTreeTestCase(unittest.TestCase):
         self.assertEqual(tree.find(8).right_node, None)
 
     def test_add_more(self):
+        """
+        Create a BinarySearchTree with root and add some elements.
+        Test that all elements added.
+        """
         tree = BinarySearchTree(root=8)
         for elem in [6, 9, 5, 7]:
             tree.add(elem)
             self.assertEqual(tree.find(elem).root, elem)
 
     def test_add_existing(self):
+        """
+        Create a BinarySearchTree with root and add an element twice.
+        Test that it returns a message.
+        """
         tree = BinarySearchTree(root=8)
         tree.add(6)
         self.assertIsInstance(tree.add(6), str)
 
     def test_binary_search_tree_find(self):
+        """
+        Create a BinarySearchTree with root and an element.
+        Test that find function finds the element correctly.
+        """
         tree = BinarySearchTree(root=8)
         tree.add(6)
         self.assertEqual(tree.find(6).root, 6)
         self.assertEqual(tree.find(8).left_node.root, 6)
 
     def test_find_all(self):
+        """
+        Create a BinarySearchTree with root and add many elements.
+        Test that all are found correctly.
+        """
         tree = BinarySearchTree(root=8)
         for elem in [4, 2, 3, 6, 1, 10, 7]:
             tree.add(elem)
@@ -51,54 +71,87 @@ class BinarySearchTreeTestCase(unittest.TestCase):
         self.assertEqual(tree.find(8).right_node.root, 10)
 
     def test_find_not_existing(self):
+        """
+        Create a BinarySearchTree with root.
+        Test that find a non-existing element returns Node with nones.
+        """
         tree = BinarySearchTree(root=8)
-        tree.add(4)
         self.assertEqual(tree.find(6).root, None)
 
     def test_remove_leaf(self):
+        """
+        Create a BinarySearchTree with root, add and remove an element.
+        Test that element is not found.
+        """
         tree = BinarySearchTree(root=8)
         tree.add(10)
         tree.remove(10)
         self.assertEqual(tree.find(10).root, None)
 
     def test_remove_branch(self):
+        """
+        Create a BinarySearchTree with root, add elements and remove one.
+        Test that you can`t find the other nodes.
+        """
         tree = BinarySearchTree(root=8)
         for elem in [6, 9, 5, 7]:
             tree.add(elem)
         tree.remove(6)
         for el in [6, 5, 7]:
             self.assertEqual(tree.find(el).root, None)
-        self.assertEqual(tree.find(9).root, 9)
 
     def test_remove_root(self):
+        """
+        Create a BinarySearchTree with root, add elements and remove root.
+        Test that you can`t find any elements.
+        """
         tree = BinarySearchTree(root=8)
         for elem in [6, 9, 5, 7]:
             tree.add(elem)
         tree.remove(8)
-        for el in [6, 9, 7]:
+        for el in [6, 9, 5, 7]:
             self.assertEqual(tree.find(el).root, None)
 
     def test_remove_not_existing(self):
+        """
+        Create a BinarySearchTree with root and try to remove non-existing element.
+        Test that it returns a message.
+        """
         tree = BinarySearchTree(root=8)
-        tree.add(10)
         self.assertIsInstance(tree.remove(9), str)
 
     def test_get_root_no_initial(self):
+        """
+        Create an empty BinarySearchTree and add an element.
+        Test that it becomes root.
+        """
         tree = BinarySearchTree()
         tree.add(7)
         self.assertEqual(tree.get_root(), 7)
 
     def test_get_root_initial(self):
+        """
+        Create a BinarySearchTree with root.
+        Test that root returns correctly.
+        """
         tree = BinarySearchTree(root=8)
         self.assertEqual(tree.get_root(), 8)
 
     def test_get_root_after_removal(self):
+        """
+        Create a BinarySearchTree with root, remove the root and add an element.
+        Test that the root is the new element.
+        """
         tree = BinarySearchTree(root=7)
         tree.remove(7)
         tree.add(9)
         self.assertEqual(tree.get_root(), 9)
 
     def test_end_to_end(self):
+        """
+        Create a BinarySearchTree with root and all the sequence.
+        Test that all functions work fine together.
+        """
         pass
 
     '''
