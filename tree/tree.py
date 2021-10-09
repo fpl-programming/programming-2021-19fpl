45507271713c113a801536b2f2dde08a21ff102b
daniilzelekson <64979241+daniilzelekson@users.noreply.github.com>
2021-10-09 11:19:54 +0300

    lab_4 pre start

---
+++ tree/tree.py
@@ -0,0 +1,47 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def get_value(self):
+        return self.value
+
+    def set_value(self, value):
+        self.value = value
+
+    def get_left(self):
+        return self.left
+
+    def set_left(self, left):
+        self.left = left
+
+    def get_right(self):
+        return self.right
+
+    def set_right(self, right):
+        self.right = right
+
+
+class Tree:
+    def __init__(self):
+        self.root = None
+        self.size = 0
+
+    def insert(self, value):
+        pass
+
+    def erase(self, value):
+        pass
+
+    def find(self, value):
+        pass
+
+    def get_size(self):
+        pass
+
+    def height(self):
+        pass
+
+    def width(self):
+        pass
