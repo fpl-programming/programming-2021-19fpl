d6464c53ab6bc7b168cf125e1c546cb9eca62156
daniilzelekson <64979241+daniilzelekson@users.noreply.github.com>
2021-10-10 17:55:01 +0300

    lab_4 lint fixes

--- tree/tree_test.py
+++ tree/tree_test.py
@@ -56,6 +56,9 @@ class TreeUnitTestCase(unittest.TestCase):
         self.assertRaises(IndexError, tree.erase, 10)
 
     def test_find(self):
+        """
+        find ent-to-end test
+        """
         tree = Tree()
         data = [x for x in range(100)]
         for i in data:
@@ -66,6 +69,9 @@ class TreeUnitTestCase(unittest.TestCase):
         self.assertTrue(tree.find(1))
 
     def test_width_greater_one(self):
+        """
+        width end-to-end test
+        """
         tree = Tree()
         tree.insert(100)
         tree.insert(150)
@@ -77,12 +83,18 @@ class TreeUnitTestCase(unittest.TestCase):
         self.assertEqual(tree.width(), 4)
 
     def test_width_eight(self):
+        """
+        another width test
+        """
         tree = Tree()
         for element in [100, 150, 50, 25, 75, 125, 175, 10, 30, 60, 80, 120, 130, 170, 180]:
             tree.insert(element)
         self.assertEqual(tree.width(), 8)
 
     def test_lot_insert_erase(self):
+        """
+        erase stress test
+        """
         tree = Tree()
         for i in range(100):
             tree.insert(i)
