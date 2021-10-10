d7de8993fc14937883a14811566b0b4bdc6281b8
katyanemzer <65305336+katyanemzer@users.noreply.github.com>
2021-10-11 00:20:39 +0300

    mod

--- binarytree/binarytree.py
+++ binarytree/binarytree.py
@@ -16,20 +16,53 @@ class BinaryTree(Node):
     def __init__(self, tree, root):
         self.root = None
 
-    def add(self, element):
-        pass
-
-    def find_recurs(self, node, key):
-
-        if self.node.data == self.key or self.node is None:
-            return self.root
-        elif self.key < self.node.data:
-            return self.find_recurs(node.left_element,key)
+    def add(self,node, element):
+        if self.root is None:
+            self.root=Node(element)
         else:
-            return self.find_recurs(node.right_element, key)
+            if node is None:
+                node=self.root
+            if element>node.element:
+                if node.right_element is None:
+                    node.right_element = Node(element)
+                self.add(node.right_element, element)
+            else:
+                if node.left.element is None:
+                    node.left_element = Node(element)
+                self.add(node.left_element, element)
 
-    def remove_recurs(self):
-        pass
+    def find(self, node, key ):
+
+        if self.root is None:
+            print ('The root is empty')
+        if node is None:
+            node = self.root
+        if node.key == key or node.left_element or node.right_element:
+            return True
+        if key < node.key and node.left_element:
+            return self.find(node.left, key)
+        if key > node.key and node.right_element:
+            return self.find(node.right_element, key)
+        return False
+
+    def remove(self, node, element):
+        if self.root is None:
+            print('The root is empty')
+        if self.find(None,element) is True:
+            if node is None:
+                node = self.root
+            if element < node.element and node.left_element:
+                if node.left_element == element:
+                    node.left_element=None
+                self.remove(node.left_element,element)
+            if element > node.element and node.right_element:
+                if element == node.right_element:
+                    node.right_element = None
+                self.remove(node.right_element,element)
+        else:
+            print("there is no node")
+
 
     def get_height(self):
         pass
+
