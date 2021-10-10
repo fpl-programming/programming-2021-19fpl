260d9d654eabf2c903a181fae61d4a36733ae947
daniilzelekson <64979241+daniilzelekson@users.noreply.github.com>
2021-10-11 00:38:41 +0300

    wrong value test added

---
+++ binary_tree/main.py
@@ -0,0 +1,169 @@
+"""
+Binary tree implementation
+"""
+
+
+class Node:
+    """
+    Tree's node structure
+    """
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def get_value(self):
+        """
+        get value
+        """
+        return self.value
+
+    def set_value(self, value):
+        """
+        sets new value
+        """
+        self.value = value
+
+    def get_left(self):
+        """
+        goes to the left column if the previous node is smaller
+        """
+        return self.left
+
+    def set_left(self, left):
+        """
+        places new node in the left column
+        """
+        self.left = left
+
+    def get_right(self):
+        """
+        goes to the left column if the previous node is bigger
+        """
+        return self.right
+
+    def set_right(self, right):
+        """
+        places new node in the right column
+        """
+        self.right = right
+
+
+class Tree:
+    """
+    Binary search tree structure
+    """
+    def __init__(self):
+        self.root = None
+        self.size = 0
+
+    def insert(self, value):
+        """
+        adds new value to the tree
+        increases tree's size
+        """
+        self.root = self._insert(self.root, value)
+        self.size += 1
+
+    def _insert(self, root, value):
+        """
+        recursive method which chooses where to place next node
+        """
+        if not isinstance(value, int):
+            raise ValueError
+
+        if root is None:
+            return Node(value)
+        if root.get_value() > value:
+            root.set_left(self._insert(root.get_left(), value))
+        else:
+            root.set_right(self._insert(root.get_right(), value))
+        return root
+
+    def erase(self, value):
+        """
+        removes tree's value
+        reduces tree's size
+        """
+        if self.find(value):
+            self.root = self._erase(self.root, value)
+            self.size -= 1
+        else:
+            raise IndexError
+
+    def _erase(self, node, value):
+        """
+        recursive erasing node method
+        """
+        if node.get_value() == value:
+            if node.get_right() is None:
+                return node.get_left()
+            node.get_right().set_left(node.get_left())
+            return node.get_right()
+        if node.get_value() > value:
+            node.set_left(self._erase(node.get_left(), value))
+        else:
+            node.set_right(self._erase(node.get_right(), value))
+        return node
+
+    def find(self, value):
+        """
+        returns the value if the tree contains it
+        """
+        return self._find(self.root, value)
+
+    def _find(self, node, value):
+        """
+        recursive finding value method
+        """
+        if node is None:
+            return False
+        if node.get_value() == value:
+            return True
+        if node.get_value() > value:
+            return self._find(node.get_left(), value)
+
+        return self._find(node.get_right(), value)
+
+    def get_size(self):
+        """
+        returns tree's size
+        """
+        return self.size
+
+    def height(self):
+        """
+        returns tree's height
+        """
+        return self._height(self.root)
+
+    def _height(self, root):
+        """
+        height finding recursive method
+        """
+        if root is None:
+            return 0
+        return 1 + max(self._height(root.get_left()), self._height(root.get_right()))
+
+    def width(self):
+        """
+        returns tree's width
+        """
+        list_now_level = []
+        if self.root is not None:
+            list_now_level.append(self.root)
+        list_next_level = []
+        result = 0
+        while len(list_now_level) != 0:
+            result = max(result, len(list_now_level))
+            for node in list_now_level:
+                if not node.get_left() is None:
+                    list_next_level.append(node.get_left())
+                if not node.get_right() is None:
+                    list_next_level.append(node.get_right())
+            list_now_level = list_next_level
+            list_next_level = []
+        return result
+
+tree = Tree()
+print(tree.width())
---
+++ binary_tree/tree_test.py
@@ -0,0 +1,122 @@
+"""
+Binary tree testing module
+"""
+
+import unittest
+from binary_tree.main import Node, Tree
+
+
+class TreeUnitTestCase(unittest.TestCase):
+    """
+    Binary tree test case
+    """
+
+    def test_insert_increase_sequence(self):
+        """
+        inserting increasing values test
+        """
+        tree = Tree()
+        for i in range(100):
+            tree.insert(i)
+
+        self.assertEqual(tree.height(), 100)
+
+    def test_insert_decrease_sequence(self):
+        """
+        inserting decreasing test
+        """
+        tree = Tree()
+        data = list(range(100))
+        data.reverse()
+        for i in data:
+            tree.insert(i)
+
+        self.assertEqual(tree.height(), 100)
+
+    def test_insert_negative(self):
+        """
+        inserting negative test
+        """
+        tree = Tree()
+        for i in range(100):
+            tree.insert(i)
+        self.assertRaises(IndexError, tree.erase, 1000)
+
+    def test_erase(self):
+        """
+        positive erase test
+        """
+        tree = Tree()
+        tree.insert(100)
+        tree.insert(150)
+        tree.insert(50)
+        tree.erase(50)
+        self.assertEqual(tree.width(), 1)
+
+    def test_erase_negative(self):
+        """
+        negative erase test
+        """
+        tree = Tree()
+        for i in range(100):
+            tree.insert(i)
+        tree.erase(10)
+        self.assertRaises(IndexError, tree.erase, 10)
+
+    def test_find(self):
+        """
+        find ent-to-end test
+        """
+        tree = Tree()
+        data = list(range(100))
+        for i in range(1, 100):
+            tree.insert(i)
+        data.reverse()
+        for i in data:
+            tree.insert(i)
+        self.assertTrue(tree.find(1))
+
+    def test_width_greater_one(self):
+        """
+        width end-to-end test
+        """
+        tree = Tree()
+        tree.insert(100)
+        tree.insert(150)
+        tree.insert(50)
+        tree.insert(25)
+        tree.insert(75)
+        tree.insert(125)
+        tree.insert(175)
+        self.assertEqual(tree.width(), 4)
+
+    def test_width_eight(self):
+        """
+        another width test
+        """
+        tree = Tree()
+        for element in [100, 150, 50, 25, 75, 125, 175, 10, 30, 60, 80, 120, 130, 170, 180]:
+            tree.insert(element)
+        self.assertEqual(tree.width(), 8)
+
+    def test_lot_insert_erase(self):
+        """
+        erase stress test
+        """
+        tree = Tree()
+        for i in range(100):
+            tree.insert(i)
+        for i in range(100):
+            tree.erase(i)
+        self.assertEqual(0, tree.get_size())
+
+    def test_wrong_input(self):
+        """
+        incorrect value input test
+        """
+        tree = Tree()
+        bad_input = ["None", 1.2345, [], {}, None]
+        for val in bad_input:
+            self.assertRaises(ValueError, tree.insert, val)
+
+
