492df3a408fba08c8d3a042f92fd15bb3219304e
Nastya1405 <frolikovanasta@gmail.com>
2021-10-11 00:01:02 +0300

    start

---
+++ Binarytree/start.py
@@ -0,0 +1,20 @@
+"""
+Programming for linguists
+Start script for BinarySearchTree
+"""
+
+from Binarytree.binarytree import BinarySearchTree
+
+if __name__ == '__main__':
+    tree = BinarySearchTree()
+    nodes = [16, 11, 24, 6, 31, 19, 1]
+    for element in nodes:
+        tree.add(element)
+
+    print(tree.get_height())
+    tree.remove(19)
+    print(tree.get_height())
+
+    RESULT = tree.find(16)
+
+    assert RESULT is True, 'Results differ'
