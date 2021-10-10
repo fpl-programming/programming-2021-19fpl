beb43ebe9e9900287331c2a62d126cfecb289f20
Nastya1405 <frolikovanasta@gmail.com>
2021-10-11 00:00:28 +0300

    tests

---
+++ Binarytree/Binarytree_tests.py
@@ -0,0 +1,78 @@
+"""
+Programming for linguists
+Tests for BinarySearchTree class
+"""
+
+
+import unittest
+
+from Binarytree.binarytree import Node, BinarySearchTree, EmptyError, NoNodeError
+
+
+class NodeTestCase(unittest.TestCase):
+    """
+        This Case of tests checks the functionality of the implementations of Node.
+        """
+
+    def test_create_new_node(self):
+        """
+        Test that node is created correctly.
+        """
+        nodes = [15, 19, 33, 12, 1, 40, 30]
+        for element in nodes:
+            node = Node(element)
+            self.assertEqual(node.element, element)
+
+    class BinarySearchTreeTestCase(unittest.TestCase):
+        """
+        This Case of tests checks the functionality of the implementations of BinarySearchTree.
+        """
+
+        def test_add_element(self):
+            """
+            Test that add function adds new element to tree.
+            """
+            tree = BinarySearchTree()
+            tree.add(20)
+            self.assertEqual(tree.find(20), True)
+
+        def test_add_existing_element(self):
+            """
+            Create a BinarySearchTree.
+            Test that add function raises ValueError when an element is already in the tree
+            """
+            tree = BinarySearchTree()
+            tree.add(15)
+            self.assertRaises(ValueError, tree.add, 15)
+
+        def test_remove(self):
+            """
+            Test that remove function removes element.
+            """
+            tree = BinarySearchTree()
+            tree.add(15)
+            self.assertEqual(tree.remove(15), None)
+
+        def test_find(self):
+            """
+            Create a BinarySearchTree.
+            Test that find function return True in case when element is found
+            """
+            tree = BinarySearchTree()
+            node = 9
+            node_left = 4
+            node_right = 2
+            tree.add(node)
+            tree.add(node_left)
+            tree.add(node_right)
+            self.assertEqual(tree.find(node_right), True)
+
+        def test_get_height(self):
+            """
+            Test that get_height function returns the max height of the tree.
+            """
+            tree = BinarySearchTree()
+            nodes = [25, 31, 33, 12, 14, 10, 40]
+            for element in nodes:
+                tree.add(element)
+            self.assertEqual(tree.get_height(), 4)
