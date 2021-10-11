61447c2cc84c0b1706c7bf8cc2c7087459b85f19
Ashfak Irem <irem.ashfak@mail.ru>
2021-10-11 11:07:04 +0300

    Create start.py

---
+++ binary_tree/start.py
@@ -0,0 +1,29 @@
+"""
+Programming for linguists
+
+Binary Tree
+
+"""
+
+from binary_search_tree.binary_search_tree import BinarySearchTree
+
+if __name__ == '__main__':
+    tree = BinarySearchTree()
+    nodes_to_add = [8, 3, 10, 1, 6]
+    for node in nodes_to_add:
+        tree.add(node)
+
+    print(f'Height of the tree is {tree.get_height()} levels\n')
+    level_values = tree.width_traverse()
+    for key, values in level_values.items():
+        print(f'Level({key}) contains these values: {values}')
+
+    tree.remove_node(3)
+
+    level_values = tree.width_traverse()
+    print('')
+    for key, values in level_values.items():
+        print(f'Level({key}) contains these values: {values}')
+
+    RESULT = tree.find(3)
+    assert RESULT is False, 'Completed successfully'
